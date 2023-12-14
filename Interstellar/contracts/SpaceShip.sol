d99b65a7103667f1fd7918dd447b2dc5ed68f5a5
Brivan-26 <km_rezazi@esi.dz>
2023-12-14 13:44:15 +0100

    Finzalized challenges

--- Interstellar/contracts/SpaceShip.sol
+++ Interstellar/contracts/SpaceShip.sol
@@ -4,27 +4,19 @@ pragma solidity ^0.7.0;
 contract SpaceShip {
     uint32 public distanceFromTheBlackHole;
     uint32 private distanceTraveled;
-    address public controlSystem;
 
     constructor() {
         distanceTraveled = 1337;
         distanceFromTheBlackHole = 4294967295;
-        controlSystem = tx.origin;
     }
 
-    function galacticBoost(uint32 advanceWithDistance) public onlyControlSystem {
+    function galacticBoost(uint32 advanceWithDistance) public {
         // Avoid the space ship from falling into the black hole
         require(distanceTraveled + advanceWithDistance < distanceFromTheBlackHole);
         distanceTraveled += advanceWithDistance;
     }
-
-    
+  
     function getDistanceTraveled() public view returns (uint256) {
-        return distanceTraveled;
-    }
-
-    modifier onlyControlSystem() {
-            require(tx.origin == controlSystem, "Only the Ground Control System can perform this action.");
-            _;
+         return distanceTraveled;
     }
 }
